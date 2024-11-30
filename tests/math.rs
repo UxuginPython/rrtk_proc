b8a5a33248e9d8d1a048d9fdbf513f4bad203453
Uxugin <git@uxugin.com>
2024-11-30 13:10:42 -0700

    3 in mul div group test

--- tests/math.rs
+++ tests/math.rs
@@ -1,7 +1,7 @@
 use rrtk::*;
 use rrtk_proc::*;
 #[test]
-fn math_test() {
+fn pid() {
     let time = rc_ref_cell_reference(Time::default());
     let kp = rc_ref_cell_reference(ConstantGetter::<u8, Time, ()>::new(time.clone(), 20));
     let ki = rc_ref_cell_reference(ConstantGetter::<u8, Time, ()>::new(time.clone(), 1));
@@ -11,4 +11,13 @@ fn math_test() {
     let drv = rc_ref_cell_reference(ConstantGetter::<u8, Time, ()>::new(time.clone(), 2));
     let pid = math!(kp * error + ki * int + kd * drv);
     assert_eq!(pid.borrow().get().unwrap().unwrap().value, 120);
+}
+#[test]
+fn three_in_mul_div_group() {
+    let time = rc_ref_cell_reference(Time::default());
+    let a = rc_ref_cell_reference(ConstantGetter::<u8, Time, ()>::new(time.clone(), 4));
+    let b = rc_ref_cell_reference(ConstantGetter::<u8, Time, ()>::new(time.clone(), 3));
+    let c = rc_ref_cell_reference(ConstantGetter::<u8, Time, ()>::new(time.clone(), 2));
+    let x = math!(a * b / c);
+    assert_eq!(x.borrow().get().unwrap().unwrap().value, 6);
 }
